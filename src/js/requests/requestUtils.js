09320a39094b708afbef7b204f82381132a1fd61
Xpload7 <js7vern@gmail.com>
2024-04-02 20:13:09 +0100

    Deleted comments inside requestUtils.js

--- src/js/requests/requestUtils.js
+++ src/js/requests/requestUtils.js
@@ -3,8 +3,6 @@
 class RequestUtils{
     static octokit = new Octokit({
         auth: import.meta.env.AUTHENTICATION_TOKEN,
-        // auth : "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"
-        // auth : "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"
     });
 }
 export default RequestUtils;
